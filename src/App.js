745b425800253589237a04ac0cfc24dd110aab1a
Bruno <brunocmoraes97@gmail.com>
2021-07-30 15:18:49 -0300

    implementa estilização ao fundo, estilizcao ao corpo, header a o nome da aplicação

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import name from './name.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-main">
+        <div className="container-header">
+          <img src={ name } alt="Nome do app" className="logoName"/>
+        </div>
+      </div>
     </div>
   );
 }
